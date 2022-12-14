759ad6f7da08a153dee0bca7f3e6090b905af7be
mananbari <manan.bari@norgic.com>
2022-12-14 10:09:08 +0500

    added node process id logs and kill process if any one process kill in concurrently

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const sendMsg = require("./controller/sendMail");
+const process = require("process");
 
 const app = express();
 const port = 7070;
@@ -9,4 +10,5 @@ app.route("/sendMessage").post(sendMsg);
 
 app.listen(port, () => {
   console.log(`server is listining on ${port}`);
+  console.log("This process is your pid " + process.pid);
 });
